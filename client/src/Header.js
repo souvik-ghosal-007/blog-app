acc3c3b22ce65416cd89512b50f65c3e2574ca3f
souvik-ghosal-007 <souvik13.12.2000@gmail.com>
2023-06-21 20:25:12 +0530

    first

--- client/src/Header.js
+++ client/src/Header.js
@@ -1,46 +1,51 @@
-import {Link} from "react-router-dom";
-import {useContext, useEffect, useState} from "react";
-import {UserContext} from "./UserContext";
+import { useContext, useEffect, useState } from "react";
+import { Link, Navigate, useNavigate } from "react-router-dom";
+import { UserContext } from "./UserContext";
 
 export default function Header() {
-  const {setUserInfo,userInfo} = useContext(UserContext);
-  useEffect(() => {
-    fetch('http://localhost:4000/profile', {
-      credentials: 'include',
-    }).then(response => {
-      response.json().then(userInfo => {
-        setUserInfo(userInfo);
-      });
-    });
-  }, []);
+    const navigate = useNavigate();
 
-  function logout() {
-    fetch('http://localhost:4000/logout', {
-      credentials: 'include',
-      method: 'POST',
-    });
-    setUserInfo(null);
-  }
+    const { setUserInfo, userInfo } = useContext(UserContext);
+    useEffect(() => {
+        fetch("http://localhost:4000/profile", {
+            credentials: "include",
+        }).then((response) => {
+            response.json().then((userInfo) => {
+                setUserInfo(userInfo);
+            });
+        });
+    }, []);
 
-  const username = userInfo?.username;
+    function logout() {
+        fetch("http://localhost:4000/logout", {
+            credentials: "include",
+            method: "POST",
+        });
+        setUserInfo(null);
+        navigate("/");
+    }
 
-  return (
-    <header>
-      <Link to="/" className="logo">MyBlog</Link>
-      <nav>
-        {username && (
-          <>
-            <Link to="/create">Create new post</Link>
-            <a onClick={logout}>Logout ({username})</a>
-          </>
-        )}
-        {!username && (
-          <>
-            <Link to="/login">Login</Link>
-            <Link to="/register">Register</Link>
-          </>
-        )}
-      </nav>
-    </header>
-  );
+    const username = userInfo?.username;
+
+    return (
+        <header>
+            <Link to="/" className="logo">
+                MyBlog
+            </Link>
+            <nav>
+                {username && (
+                    <>
+                        <Link to="/create">Create new post</Link>
+                        <a onClick={logout}>Logout ({username})</a>
+                    </>
+                )}
+                {!username && (
+                    <>
+                        <Link to="/login">Login</Link>
+                        <Link to="/register">Register</Link>
+                    </>
+                )}
+            </nav>
+        </header>
+    );
 }
--- client/src/Post.js
+++ client/src/Post.js
@@ -1,25 +1,43 @@
-import {formatISO9075} from "date-fns";
-import {Link} from "react-router-dom";
+import { format } from "date-fns";
+import { Link } from "react-router-dom";
 
-export default function Post({_id,title,summary,cover,content,createdAt,author}) {
+export default function Post({
+    _id,
+    title,
+    summary,
+    cover,
+    content,
+    createdAt,
+    author,
+}) {
+    return (
+        <div className="post">
+            <div className="image">
+                <Link to={`/post/${_id}`}>
+                    <img
+                        src={"http://localhost:4000/" + cover}
+                        alt=""
+                        style={{
+                            height: "250px",
+                            width: "500px",
+                            objectFit: "cover",
+                        }}
+                    />
+                </Link>
+            </div>
+            <div className="texts">
+                <Link to={`/post/${_id}`}>
+                    <h2>{title}</h2>
+                </Link>
 
-  return (
-    <div className="post">
-      <div className="image">
-        <Link to={`/post/${_id}`}>
-          <img src={'http://localhost:4000/'+cover} alt=""/>
-        </Link>
-      </div>
-      <div className="texts">
-        <Link to={`/post/${_id}`}>
-        <h2>{title}</h2>
-        </Link>
-        <p className="info">
-          <a className="author">{author.username}</a>
-          <time>{formatISO9075(new Date(createdAt))}</time>
-        </p>
-        <p className="summary">{summary}</p>
-      </div>
-    </div>
-  );
+                <p className="info">
+                    <a className="author">{author.username}</a>
+                    <time>
+                        {format(new Date(createdAt), "MMM yy, hh:mm aaa")}
+                    </time>
+                </p>
+                <p className="summary">{summary}</p>
+            </div>
+        </div>
+    );
 }
